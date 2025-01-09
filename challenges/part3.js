8e578790d22b2b87ca7f16458070488157cf7518
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-01-09 16:21:50 -0300

    3rd add nested elements, semivisual code and vDOM

--- challenges/part3.js
+++ challenges/part3.js
@@ -1,26 +1,63 @@
 let myName = "";
-let isFocus = false;
-
 let jsInput;
 let jsDiv;
+let vDOM;
+let focusedInInput;
 
-function handleInput() {
-	myName = jsInput.value;
+function createVDOM() {
+	return [
+		[
+			"input",
+			myName,
+			function handle() {
+				myName = jsInput.value;
+			},
+		],
+		[
+			"div",
+			`Hello, ${myName}!`,
+			,
+			[
+				["div", "nested 1"],
+				["div", "nested 2"],
+				["div", `nested 3 - user: ${myName}`],
+			],
+		],
+	];
 }
 
-function component() {
-	document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
+function convert(node) {
+	const [element, content, handler, children] = node;
+	const el = document.createElement(element);
+	el.textContent = content;
+	el.value = content;
+	el.oninput = handler;
 
-	jsInput = document.createElement("input");
-	jsInput.value = myName;
-	jsInput.oninput = handleInput;
-	
-  jsDiv = document.createElement("div");
-	jsDiv.textContent = myName;
+	if (children && children.length > 0) {
+		for (const child of children) {
+			const [element, content, handler] = child;
 
-  document.body.replaceChildren(jsInput, jsDiv)
+			const childEl = document.createElement(element);
+			childEl.textContent = content;
+			childEl.value = content;
+			childEl.oninput = handler;
 
-	if (isFocus) jsInput.focus();
+			el.appendChild(childEl);
+		}
+	}
+
+	return el;
 }
 
-setInterval(component, 15);
+function updateDOM() {
+	focusedInInput = document.activeElement === jsInput;
+
+	vDOM = createVDOM();
+	jsInput = convert(vDOM[0]);
+	jsDiv = convert(vDOM[1]);
+	document.body.replaceChildren(jsInput, jsDiv);
+
+	if (focusedInInput) jsInput.focus();
+}
+
+setInterval(updateDOM, 15);
