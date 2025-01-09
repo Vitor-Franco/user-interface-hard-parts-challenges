7c8e2cae5ed41ff8f98e8b52594040a98e21c16c
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-01-09 16:37:36 -0300

    4rd part - add function components and flexible dom with composition

--- challenges/part4.js
+++ challenges/part4.js
@@ -1,62 +1,73 @@
-let myName = "";
+// let myName = "";
 
-const vDOM = [
-  [
-    "input",
-    myName,
-    function handle() {
-      myName = jsInput.value;
-    },
-  ],
-  ["div", `Hello, ${myName}!`],
-];
+// const vDOM = [
+// 	[
+// 		"input",
+// 		myName,
+// 		function handle() {
+// 			myName = jsInput.value;
+// 		},
+// 	],
+// 	["div", `Hello, ${myName}!`],
+// ];
 
-function convert(node) {
-  const element = document.createElement(node[0]);
-  element.textContent = node[1];
-  element.value = node[1];
-  element.oninput = node[2];
-  return element;
-}
+// function convert(node) {
+// 	const element = document.createElement(node[0]);
+// 	element.textContent = node[1];
+// 	element.value = node[1];
+// 	element.oninput = node[2];
+// 	return element;
+// }
 
 /** Step @todo */
 /* uncomment the code below this line, and comment out the code above*/
 
-// let isFocus = false
+let isFocus = false;
 
-// let myName = '';
-// let jsInput;
-// let jsDiv;
-// let vDOM;
+let myName = "";
+let vDOM;
+let elems = [];
 
-// function createVDOM() {
-//   return [
-//     [
-//       'input',
-//       myName,
-//       function handle() {
-//         myName = jsInput.value;
-//       },
-//     ],
-//     ['div', `Hello, ${myName}!`],
-//   ];
-// }
+function HelloUser(username) {
+  if (username.length < 4) {
+    return ['div', 'Name is too short']
+  }
 
-// function updateDOM() {
-//   document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
-//   vDOM = createVDOM();
-//   jsInput = convert(vDOM[0]);
-//   jsDiv = convert(vDOM[1]);
-//   document.body.replaceChildren(jsInput, jsDiv);
-//   if(isFocus) jsInput.focus()
-// }
+	return ["div", `Hello, ${username}!`];
+}
 
-// function convert(node) {
-//     const element = document.createElement(node[0]);
-//     element.textContent = node[1];
-//     element.value = node[1];
-//     element.oninput = node[2];
-//     return element;
-//   }
+function createVDOM() {
+	return [
+		[
+			"input",
+			myName,
+			function handle(e) {
+				myName = e.target.value;
+			},
+		],
+		HelloUser(myName),
+		HelloUser(myName),
+		HelloUser(myName),
+		HelloUser(myName),
+		HelloUser(myName),
+		HelloUser(myName),
+	];
+}
 
-// setInterval(updateDOM, 15);
+function updateDOM() {
+	document.activeElement === elems[0] ? (isFocus = true) : (isFocus = false);
+	vDOM = createVDOM();
+	elems = vDOM.map(convert);
+	document.body.replaceChildren(...elems);
+	if (isFocus) elems[0].focus();
+}
+
+function convert(node) {
+	const element = document.createElement(node[0]);
+	element.textContent = node[1];
+	element.value = node[1];
+	element.oninput = node[2];
+	return element;
+}
+
+setInterval(updateDOM, 15);
