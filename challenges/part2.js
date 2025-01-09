a9323d30c1495814f6606b9dcc50b995d9ecb2fc
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-01-09 15:59:39 -0300

    solve 2th part

--- challenges/part2.js
+++ challenges/part2.js
@@ -1,11 +1,31 @@
-let post = '';
+let post = "";
+const posts = [];
 
-const jsInput = document.querySelector('input');
-const jsDiv = document.querySelector('div');
+const jsInput = document.querySelector("input");
+const jsDiv = document.querySelector("div");
+const jsButton = document.querySelector("button");
 
 function handleInput() {
-  post = jsInput.value;
-  jsDiv.textContent = post;
+	post = jsInput.value;
 }
 
+function addPost() {
+	posts.push(post);
+	post = "";
+}
+
+function dataToView() {
+	jsInput.value = post;
+
+	const postsElem = posts.map((post) => {
+		const newPost = document.createElement("div");
+		newPost.textContent = post;
+		return newPost;
+	});
+	jsDiv.replaceChildren(...postsElem);
+}
+
+setInterval(dataToView, 15);
+
 jsInput.oninput = handleInput;
+jsButton.onclick = addPost;
