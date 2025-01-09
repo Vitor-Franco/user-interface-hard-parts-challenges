9ee2fa3cf83da987eb6e9d14f755d57795876cd1
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-01-09 15:45:06 -0300

    solve first exercise

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,15 @@
 /** Write your code below */
+console.log("document", document);
+
+let post = "";
+const jsInput = document.querySelector("input");
+
+const jsDiv = document.querySelector("div");
+jsDiv.textContent = post;
+
+function handleInput(e) {
+	post = e.target.value;
+	jsDiv.textContent = post;
+}
+
+jsInput.oninput = handleInput;
