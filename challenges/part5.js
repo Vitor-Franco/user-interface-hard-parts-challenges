859644667265ba7983e98f0a651606da1991a8c4
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-01-09 16:56:00 -0300

    fifth part - add hooks, simple reconciliation and diffing algorithms

--- challenges/part5.js
+++ challenges/part5.js
@@ -1,48 +1,59 @@
-let myName = "";
+const data = {
+	myName: "",
+};
+let prevVDOM;
 let vDOM;
 let elems;
 
 function createVDOM() {
-  return [
-    [
-      "input",
-      myName,
-      function handle(e) {
-        myName = e.target.value;
-      },
-    ],
-    ["div", `Hello, ${myName}!`],
-    ["div", `Great job, Jonathan!`],
-    ["div", `Great job, Alexa!`],
-    ["div", `Great job, Emilia!`],
-  ];
+	return [
+		[
+			"input",
+			data.myName,
+			function handle(e) {
+				const value = e.target.value;
+				updateData("myName", value);
+			},
+		],
+		["div", `Hello, ${data.myName}!`],
+		["div", "Great job, Jonathan!"],
+		["div", "Great job, Alexa!"],
+		["div", "Great job, Emilia!"],
+	];
+}
+
+function updateData(label, value) {
+	data[label] = value;
+	requestAnimationFrame(updateDOM);
 }
 
 function updateDOM() {
-  if (vDOM)
-    document.activeElement == document.querySelector("input")
-      ? (isFocus = true)
-      : (isFocus = false); // keep this code
-  vDOM = createVDOM();
-  elems = vDOM.map(convert);
-  document.body.replaceChildren(...elems);
-  if (isFocus) elems[0].focus(); //keep this code
+	if (!elems) {
+		vDOM = createVDOM();
+		elems = vDOM.map(convert);
+		document.body.replaceChildren(...elems);
+	} else {
+		prevVDOM = [...vDOM];
+		vDOM = createVDOM();
+		findDiff(prevVDOM, vDOM);
+	}
 }
 
 function convert(node) {
-  const element = document.createElement(node[0]);
-  element.textContent = node[1];
-  element.value = node[1];
-  element.oninput = node[2];
-  return element;
+	const element = document.createElement(node[0]);
+	element.textContent = node[1];
+	element.value = node[1];
+	element.oninput = node[2];
+	return element;
 }
 
-// function findDiff(prevVDOM, currentVDOM) {
-//     for (let i = 0; i < currentVDOM.length; i++) {
-//         if(JSON.stringify(prevVDOM[i]) !== JSON.stringify(currentVDOM[i])){
-//             // change the actual DOM element related to that vDOM element!
-//         }
-//     }
-// }
+function findDiff(prevVDOM, currentVDOM) {
+	for (let i = 0; i < currentVDOM.length; i++) {
+		if (JSON.stringify(prevVDOM[i]) !== JSON.stringify(currentVDOM[i])) {
+			elems[i].textContent = currentVDOM[i][1];
+			elems[i].value = currentVDOM[i][1];
+		}
+	}
+}
 
-setInterval(updateDOM, 15);
+updateDOM();
